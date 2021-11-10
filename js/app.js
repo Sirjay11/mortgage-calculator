78b337a4168bfebdfcd8aca54728a3c0c7d23659
James-Leroy <jlanvja@gmail.com>
2021-11-10 09:59:32 -0500

    update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//variable for 
+//variable for
 let state = {
   price: getNumber(document.querySelectorAll('[name="price"]')[0].value),
   loan_years: document.querySelectorAll('[name="loan_years"]')[0].value,
@@ -8,7 +8,7 @@ let state = {
   home_insurance: document.querySelectorAll('[name="home_insurance"]')[0].value,
   hoa: document.querySelectorAll('[name="hoa"]')[0].value,
 };
-
+//declared variables
 let totalLoan,
   totalMonths,
   monthlyInterest,
@@ -16,6 +16,7 @@ let totalLoan,
   monthlyPropertyTaxes,
   monthlyHomeInsurance,
   monthlyHOA,
+  monthlyTotal,
   labels = ["Principal & Interest", "Property Tax", "Home Insurance", "HOA"],
   backgroundColor = [
     "rgb(255, 99, 132, 1)",
@@ -63,9 +64,9 @@ myChart.options.animation = false;
 
 //add event listener to inputs
 let i;
-let inputTexts = document.getElementsByClassName('form-group__textInput');
+let inputTexts = document.getElementsByClassName("form-group__textInput");
 for (i = 0; i < inputTexts.length; i++) {
- inputTexts[i].addEventListener('input', updateInputsState)
+  inputTexts[i].addEventListener("input", updateInputsState);
 }
 
 let inputSlides = document.getElementsByClassName("form-group__range-slide");
@@ -74,25 +75,80 @@ for (i = 0; i < inputSlides.length; i++) {
 }
 
 function updateInputsState(event) {
- let name = event.target.name;
- let value = event.target.value;
- if(name == 'price'){
-  value = getNumber(value);
- }
- if(event.target.type == 'range'){
-  let total = (document.getElementsByClassName(`total__${name}`))[0].innerHTML = `${value}`
- }
- state = {
-  ...state,
-  [name]: value
- }
- console.log(state)
+  let name = event.target.name;
+  let value = event.target.value;
+  if (name == "price") {
+    value = getNumber(value);
+  }
+  if (event.target.type == "range") {
+    let total = (document.getElementsByClassName(
+      `total__${name}`
+    )[0].innerHTML = `${value}`);
+  }
+  state = {
+    ...state,
+    [name]: value,
+  };
+  calculateData();
 }
 
-document.getElementsByTagName('form')[0].addEventListener('submit', (event) => {
- event.preventDefault();
- document
-   .getElementsByClassName("mg-page__right")[0]
-   .classList.add("mg-page__right--animate");
-})
-console.log(inputTexts);
+document.getElementsByTagName("form")[0].addEventListener("submit", (event) => {
+  event.preventDefault();
+  document
+    .getElementsByClassName("mg-page__right")[0]
+    .classList.add("mg-page__right--animate");
+  calculateData();
+});
+
+function calculateData() {
+  totalLoan = state.price - state.price * (state.down_payment / 100);
+  totalMonths = state.loan_years * 12;
+  monthlyInterest = state.interest_rate / 100 / 12;
+  monthlyPrincipalInterest = (
+    (totalLoan * (monthlyInterest * (1 + monthlyInterest) ** totalMonths)) /
+    ((1 + monthlyInterest) ** totalMonths - 1)
+  ).toFixed(2);
+  monthlyPropertyTaxes = (
+    (state.price * (state.property_tax / 100)) /
+    12
+  ).toFixed(2);
+  monthlyHomeInsurance = state.home_insurance / 12;
+  monthlyHOA = state.hoa / 12;
+  monthlyTotal =
+    parseFloat(monthlyPrincipalInterest) +
+    parseFloat(monthlyPropertyTaxes) +
+    parseFloat(monthlyHomeInsurance) +
+    parseFloat(monthlyHOA);
+
+  document.getElementsByClassName("info__numbers--principal")[0].innerHTML =
+    parseFloat(monthlyPrincipalInterest).toFixed(2);
+  document.getElementsByClassName(
+    "info__numbers--property_taxes"
+  )[0].innerHTML = parseFloat(monthlyPropertyTaxes).toFixed(2);
+  document.getElementsByClassName(
+    "info__numbers--home_insurance"
+  )[0].innerHTML = parseFloat(monthlyHomeInsurance).toFixed(2);
+  document.getElementsByClassName("info__numbers--hoa")[0].innerHTML =
+    parseFloat(monthlyHOA).toFixed(2);
+  document.getElementsByClassName("info__numbers--total")[0].innerHTML =
+    monthlyTotal.toFixed(2);
+  updateChart(myChart, labels, backgroundColor);
+}
+
+function updateChart(chart, label, color) {
+  chart.data.datasets.pop();
+  chart.data.datasets.push({
+    label: label,
+    backgroundColor: color,
+    data: [
+      monthlyPrincipalInterest,
+      monthlyPropertyTaxes,
+      monthlyHomeInsurance,
+      monthlyHOA,
+    ],
+  });
+  chart.options.transitions.active.animation.duration = 0;
+  chart.update()
+}
+
+calculateData()
